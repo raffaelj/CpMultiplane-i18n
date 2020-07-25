f24706f76c3b0d18804d0c23489768f2db17697e
Raffael <github@raffaeljesche.de>
2020-07-25 19:49:46 +0200

    started to create i18n repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# CpMultiplane i18n
+
+translation files for [CpMultiplane][1]
+
+
+[1]: https://github.com/raffaelj/CpMultiplane
---
+++ fr.php
@@ -0,0 +1,71 @@
+<?php return [
+
+    '@dateformat' => 'd.m.Y',
+    '@dateformat_long' => 'd.m.Y H:i',
+
+    'Something went wrong. This site doesn\'t exist.' => 'Erreur. Cette page n\'existe pas.',
+    'Page not found' => 'Page introuvable',
+    'Back to start' => 'Retour à la page d\'accueil',
+    'Something went wrong' => 'Erreur',
+    'Contact Form' => 'Formulaire de contact',
+    'Contact Me' => 'Contact',
+    'since' => 'depuis',
+    'built with' => 'réalisé avec',
+
+    // messages from Forms
+    'Please fill in all mandatory fields correctly.' => 'Complétez correctement les champs obligatoires, s\'il vous plaît.',
+    'Thank you for your message. I will answer soon.' => 'Merci pour votre message. Nous allons vous répondre dans les plus brefs délais.',
+    'Your mail wasn\'t sent correctly.' => 'Votre message n\'a pu être envoyé correctement.',
+    'Send' => 'Envoyer',
+
+    // FormValidation addon
+    'is required' => 'est un champ obligatoire',
+    'does not exist' => 'n\'existe pas',
+    'Hello spambot' => 'Coucou spambot',
+    'must be mail' => 'Inscrivez une adresse mail valide, s\'il vous plaît.',
+    'must be phone' => 'Inscrivez un numéro de téléphone valide, s\'il vous plaît.',
+    'must be url' => 'doit être une url',
+    'must not be mail' => 'N\'inscirvez pas d\'adresse mail dans ce champ (pour éviter les spams).',
+    'must not be phone' => 'N\'inscirvez pas de numéro de téléphone dans ce champ (pour éviter les spams).',
+    'must not be url' => 'N\'inscirvez pas d\'url dans ce champ (pour éviter les spams).',
+    'doesn\'t match' => 'pas correcte',
+
+    // form field labels and infos
+    'Name' => 'Nom',
+    'Surname' => 'Prénom',
+    'Telephone' => 'Téléphone',
+    'Mail Address' => 'Adresse Mail',
+    'Subject' => 'Objet',
+    'Message' => 'Message',
+    'I read the privacy notice' => 'J\'ai lu la charte de confidentialité',
+    'When using the contact form, some data will be collected and stored - see also' => 'Quelques données sont collectées pendant l\'utilisation du formulaire de contact - voir',
+
+    // other
+    'back to start page' => 'Retour à la page d\'accueil',
+    'All' => 'Tous',
+    'Search' => 'Rechercher',
+    'result' => 'Résultat',
+    'results' => 'Résultats',
+    'read more...' => 'Lire la suite...',
+    'read more' => 'Lire la suite',
+
+    // privacy notice
+    'Accept' => 'Accepter',
+    'No' => 'Non',
+    'Privacy notice' => 'Charte de confidentialité',
+    'To watch embedded videos on this page, they have to be loaded from a third party. At this moment, the platform owners will collect personal data about you.' => 'Pour regarder les vidéos, elles doivent être chargées. À partir de là, les gérant.e.s de la plateforme collectent des données personelles.',
+    'To prevent you from accepting this message over and over again, a so-called cookie will be set.' => 'Pour vous éviter de confirmer ce message à chaque reprise, nous utilisons des cookies.',
+    'You can find details in my' => 'Vous trouverez des détails dans notre',
+    'privacy notice' => 'Charte de confidentialité',
+    'Allow loading external media from YouTube/Vimeo' => 'Autoriser le chargement de vidéos externes',
+
+    // pagination
+    'next' => 'suivante',
+    'previous' => 'précédente',
+    'first' => 'première',
+    'last' => 'dernière',
+    'page' => 'page',
+    'Page' => 'page',
+    'of' => 'de',
+
+];
